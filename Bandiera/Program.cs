51eea451433cd7260d58552e27c4c6f21da6bde9
Anny Bevilacqua <anny.bevilacqua@studio.unibo.it>
2023-06-17 11:18:32 +0200

    Refactor project
    Create a single solution for the whole package and deletes the other ones.

--- Bandiera/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Bandiera;
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
---
+++ Bevilacqua/gamemap/IRoom.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP22_rusco_dc_csharp.Bevilacqua.gamemap
+{
+    internal interface IRoom
+    {
+    }
+}
