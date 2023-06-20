3d4e5685af2b95fae6dd2f3aba5a204f78b5a469
Jacopo Pesaresi <jacopo.pesaresi2@studio.unibo.it>
2023-06-20 23:54:57 +0200

    Add some new movement Also added a "dummy Main", a class to avoid a c# compilation error ("missing Main class")

---
+++ DummyMain.cs
@@ -0,0 +1,10 @@
+namespace OOP22_rusco_dc_csharp
+{
+    public class DummyMain
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello everybody!");
+        }
+    }
+}
--- Pesaresi/Gamecommand/GameControl.cs
+++ Pesaresi/Gamecommand/GameControl.cs
@@ -2,6 +2,7 @@ namespace OOP22_rusco_dc_csharp.Pesaresi.Gamecommand
 {
     public enum GameControl
     {
+        MOVE,
         //dummy implementation
     }
 }
